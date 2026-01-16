7a8d7088c225a9fde65a172fb94b914a0b4b4534
ltica-sdf <xtica2@rrc.ca>
2026-01-16 09:54:22 -0600

    Renders html based on data set

--- src/components/common/nav/Nav.tsx
+++ src/components/common/nav/Nav.tsx
@@ -1,19 +1,23 @@
 import "./Nav.css";
 
 function Nav() {
-    return(<nav>
-         <div className="page-links">
-            <span>
-                <a href="#">STORE</a>
-            </span>
-            <span>
-                <a href="#">LIBRARY</a>
-            </span>
-            <span>
-                <a href="#">PROFILE</a>
-            </span>
-        </div>
-    </nav>);
+    const tabs = [
+        { tab: "STORE", href: "#" },
+        { tab: "LIBRARY", href: "#" },
+        { tab: "PROFILE", href: "#" }
+    ];
+
+    return (
+        <nav>
+            <div className="page-links">
+                {tabs.map((tab, index) => (
+                    <span key={index}>
+                        <a href={tab.href}>{tab.tab}</a>
+                    </span>
+                ))}
+            </div>
+        </nav>
+    );
 }
 
 export default Nav;
