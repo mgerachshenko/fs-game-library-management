41a481fef7f3c08261985d594d011b3f0b31d2c0
rrc_mgerachshenko <mgerachshenko@academic.rrc.ca>
2026-02-26 23:17:33 -0600

    Search results now have game cards, and fixed colour of toolbar

--- src/components/pages/StorePage/StorePage.tsx
+++ src/components/pages/StorePage/StorePage.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import FeaturedGames from "./FeaturedGames/FeaturedGames";
 import StoreToolbar from "./StoreToolbar/StoreToolbar";
 import { useGames } from "../../../hooks/useGames";
+import type { Game } from "../../../types/game.ts";
 
 type ReviewsByGame = { [id: number]: string[] };
 
@@ -32,12 +33,27 @@ function StorePage() {
                     setReviewsByGame={setReviewsByGame}
                 />
             )}
-            {isSearching &&
-                games.map((game) => (
-                    <div key={game.id}>
-                        <h3>{game.title}</h3>
-                    </div>
-                ))}
+            {isSearching && (
+                <section className="featured-games">
+                    <h2>Search Results</h2>
+                    <ul className="featured-games-list" tabIndex={0}>
+                        {games.map((game: Game) => (
+                            <li
+                                key={game.id}
+                                className="featured-game-card"
+                            >
+                                <img
+                                    src={game.image}
+                                    alt={game.title}
+                                />
+                                <span className="game-title">
+                                    {game.title}
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            )}
         </>
     );
 }
